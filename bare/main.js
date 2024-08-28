82d8193a25e6d538a2f1d7253180fd950a52b5a5
Fred <gasolin@gmail.com>
2024-08-28 15:24:13 +0800

    [DOC] add short tutorial

--- bare/main.js
+++ bare/main.js
@@ -6,11 +6,21 @@ const ce = require('compact-encoding')
 const rpc = new RPC(HelloBare.sendMessage)
 HelloBare.onMessage = rpc.recv.bind(rpc)
 
-rpc.register(0, {
+let methodCount = 0
+
+// reverseString
+rpc.register(methodCount++, {
   request: ce.string,
   response: ce.string,
   onrequest: message => message.split('').reverse().join('')
 })
+
+// upperString
+// rpc.register(methodCount++, {
+//   request: ce.string,
+//   response: ce.string,
+//   onrequest: message => message.toUpperCase()
+// })
 
 // keep the event loop alive
 setInterval(() => {}, 2000)
