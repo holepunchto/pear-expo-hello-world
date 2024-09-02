cb652b5b9bf26ee88e61edb8ea8e39b571dedc3c
Luís Moromisato <moromisato.luis@gmail.com>
2024-09-01 22:55:35 -0300

    chore: upgrade expo to sdk 51 (#19)

--- modules/hello-bare/android/src/main/java/to/holepunch/hellobare/HelloBareModule.kt
+++ modules/hello-bare/android/src/main/java/to/holepunch/hellobare/HelloBareModule.kt
@@ -2,11 +2,13 @@ package to.holepunch.hellopear
 
 import com.facebook.react.bridge.CatalystInstanceImpl
 import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.common.annotations.FrameworkAPI
 import expo.modules.kotlin.modules.Module
 import expo.modules.kotlin.modules.ModuleDefinition
 
 @Suppress("unused")
 class HelloBareModule : Module() {
+  @OptIn(FrameworkAPI::class)
   override fun definition() = ModuleDefinition {
     Name("HelloBare")
 
